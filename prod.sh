60a20ec722a4719017b45569c983f438472efb2d
AghaRameez1 <agharameez1990@gmail.com>
2020-09-21 12:04:59 +0500

    Adding Production Build

---
+++ prod.sh
@@ -0,0 +1,4 @@
+REPOSITORY_NAME="ddkoin/rest-api-client"
+TAG=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
+
+docker build -f prod.Dockerfile -t $REPOSITORY_NAME:$TAG .
