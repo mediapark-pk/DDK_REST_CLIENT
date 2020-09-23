74d336ba967b6729af9bf20079cadbb8c0430d10
AghaRameez1 <agharameez1990@gmail.com>
2020-09-23 17:28:22 +0500

    Fixing errors

--- src/controller/block.ts
+++ src/controller/block.ts
@@ -19,31 +19,32 @@ export class BlockController {
     }
 
     @validate
-    async getMany(req: Request, res: Response) {
+    getMany(req: Request, res: Response) {
         let limit = req.body.limit
-        let offset = req.body.offset        
+        let offset = req.body.offset  
         let response = null
+        let data2 = null;
         // const response = await nodePool
         //     .send(API_ACTION_TYPES.GET_BLOCKS, req.body);
         if (limit <= 100 && offset <= 100){
-        const ws = new WebSocket('ws://185.244.248.16:4903/');
-        ws.on('open', function open(){
+            let ws = new WebSocket('ws://185.244.248.16:4903/');
+            ws.on('open', function open(){
                 ws.send(`{"event":"message","data":{"code":"GET_BLOCKS","headers":{"id":"ef2aad70-3f84-4250-895b-bb32ce4c6c1b","type":"request"},"body":{"paginator":{"limit":${limit},"offset":${offset}},"sort":[["createdAt","desc"]]}}}`);
-        });
-        await ws.on('message', function incoming(event){
-            response = JSON.parse(event.toString())
-            const data2 = {
-                "success": true, 
-                "data" :{
-                    "blocks": response.data.body.data.data,
-                },
-                "count": response.data.body.data.totalCount
-            }
-            ws.close();
-            return res.send(data2);
-        });
+            });
+            ws.on('message', function incoming(event) {                
+                response = JSON.parse(event.toString());
+                    data2 = {
+                        "success": true,
+                        "data": {
+                            "blocks": response.data.body.data.data,
+                        },
+                        "count": response.data.body.data.totalCount
+                    };
+                ws.close();
+                return res.send(data2);
+            });
         }else{
-            const data2={
+            data2={
                 "success": false,
                 "errors": [
                     "Invalid arguments",
@@ -51,9 +52,8 @@ export class BlockController {
                     "Missing required property: limit"
                 ]
             }
-            return res.send(data2)
         }
-        return res.send('hello')
+        // return res.send(data2);
     }
 
     async getLast(_req: Request, res: Response): Promise<Response> {
