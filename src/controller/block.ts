3c6fe837936d46758304c88d26a0df8b858f2914
AghaRameez1 <agharameez1990@gmail.com>
2020-09-23 14:24:06 +0500

    debugging blocks api

--- src/controller/block.ts
+++ src/controller/block.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
 import { API_ACTION_TYPES } from 'ddk.registry/dist/model/transport/code';
 import { Block } from 'ddk.registry/dist/model/common/block';
-
 import { validate } from 'src/util/validate';
 import { nodePool } from 'src/service';
+import WebSocket = require('ws')
 
 export class BlockController {
     constructor() {
@@ -19,11 +19,40 @@ export class BlockController {
     }
 
     @validate
-    async getMany(req: Request, res: Response): Promise<Response> {
-        const response = await nodePool
-            .send(API_ACTION_TYPES.GET_BLOCKS, req.body);
-
-        return res.send(response);
+    async getMany(req: Request, res: Response) {
+        let limit = req.body.limit
+        let offset = req.body.offset        
+        let response = null
+        // const response = await nodePool
+        //     .send(API_ACTION_TYPES.GET_BLOCKS, req.body);
+        if (limit <= 100 && offset <= 100){
+        const ws = new WebSocket('ws://185.244.248.16:4903/');
+        ws.on('open', function open(){
+                ws.send(`{"event":"message","data":{"code":"GET_BLOCKS","headers":{"id":"ef2aad70-3f84-4250-895b-bb32ce4c6c1b","type":"request"},"body":{"paginator":{"limit":${limit},"offset":${offset}},"sort":[["createdAt","desc"]]}}}`);
+        });
+        await ws.on('message', function incoming(event){
+            response = JSON.parse(event.toString())
+            const data2 = {
+                "success": true, 
+                "data" :{
+                    "blocks": response.data.body.data.data,
+                },
+                "count": response.data.body.data.totalCount
+            }
+            ws.close();
+            return res.send(data2);
+        });
+        }else{
+            const data2={
+                "success": false,
+                "errors": [
+                    "Invalid arguments",
+                    "Missing required property: offset",
+                    "Missing required property: limit"
+                ]
+            }
+            return res.send(data2)
+        }
     }
 
     async getLast(_req: Request, res: Response): Promise<Response> {
