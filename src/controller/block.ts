c8d4f71c39acf30575c3bf3202eb80ba1eae80a7
AghaRameez1 <agharameez1990@gmail.com>
2020-09-24 17:22:51 +0500

    adding bypass for transactions and blocks request

--- src/controller/block.ts
+++ src/controller/block.ts
@@ -24,12 +24,21 @@ export class BlockController {
         let offset = req.body.offset  
         let response = null
         let data2 = null;
+        let sort = null;
+        if (req.body.sort){
+            sort = req.body.sort
+            sort = `"${sort[0][0]}","${sort[0][1]}"`
+        }
         // const response = await nodePool
         //     .send(API_ACTION_TYPES.GET_BLOCKS, req.body);
         if (limit <= 100 && offset <= 100){
             let ws = new WebSocket('ws://185.244.248.16:4903/');
             ws.on('open', function open(){
-                ws.send(`{"event":"message","data":{"code":"GET_BLOCKS","headers":{"id":"ef2aad70-3f84-4250-895b-bb32ce4c6c1b","type":"request"},"body":{"paginator":{"limit":${limit},"offset":${offset}},"sort":[["createdAt","desc"]]}}}`);
+                if(sort != null){
+                ws.send(`{"event":"message","data":{"code":"GET_TRANSACTIONS","headers":{"id":"652f52d3-b2c1-43be-9307-1e28bb9c5c66","type":"request"},"body":{"paginator":{"limit":10,"offset":0},"sort":[[${sort}]]}}}`);
+                }else{
+                ws.send(`{"event":"message","data":{"code":"GET_BLOCKS","headers":{"id":"3c5ec5a2-993c-41c8-b082-882bb69d5a16","type":"request"},"body":{"paginator":{"limit":${limit},"offset":${offset}},"sort":[["createdAt","desc"]]}}}`);
+                }
             });
             ws.on('message', function incoming(event) {                
                 response = JSON.parse(event.toString());
--- src/controller/transaction.ts
+++ src/controller/transaction.ts
@@ -9,6 +9,8 @@ import { validate } from 'src/util/validate';
 import { transactionService } from 'src/service';
 import { transactionRepository } from 'src/repository';
 import { HTTP_STATUS } from 'src/util/http';
+import WebSocket = require('ws')
+
 
 export class TransactionController {
     @validate
@@ -30,13 +32,56 @@ export class TransactionController {
     }
 
     @validate
-    async getMany(req: Request, res: Response): Promise<Response> {
-        const response = await nodePool.send(
-            API_ACTION_TYPES.GET_TRANSACTIONS,
-            req.body,
-        );
+    getMany(req: Request, res: Response) {
+        // const response = await nodePool.send(
+        //     API_ACTION_TYPES.GET_TRANSACTIONS,
+        //     req.body,
+        // );
 
-        return res.send(response);
+        // return res.send(response);
+        let limit = req.body.limit
+        let offset = req.body.offset  
+        let response = null
+        let data2 = null;
+        let sort = null;
+        if (req.body.sort){
+            sort = req.body.sort
+            sort = `"${sort[0][0]}","${sort[0][1]}"`
+        }        
+        // const response = await nodePool
+        //     .send(API_ACTION_TYPES.GET_BLOCKS, req.body);
+        if (limit <= 100 && offset <= 100){
+            let ws = new WebSocket('ws://185.244.248.16:4903/');
+            ws.on('open', function open(){
+                if(sort != null){
+                    ws.send(`{"event":"message","data":{"code":"GET_TRANSACTIONS","headers":{"id":"652f52d3-b2c1-43be-9307-1e28bb9c5c66","type":"request"},"body":{"paginator":{"limit":10,"offset":0},"sort":[[${sort}]]}}}`);
+                }else{
+                ws.send(`{"event":"message","data":{"code":"GET_TRANSACTIONS","headers":{"id":"652f52d3-b2c1-43be-9307-1e28bb9c5c66","type":"request"},"body":{"paginator":{"limit":${limit},"offset":${offset}}}}}`);
+                }
+            });
+            ws.on('message', function incoming(event) {                
+                response = JSON.parse(event.toString());
+                    data2 = {
+                        "success": true,
+                        "data": {
+                            "transactions": response.data.body.data.data,
+                        },
+                        "count": response.data.body.data.totalCount
+                    };
+                ws.close();
+                return res.send(data2);
+            });
+        }else{
+            data2={
+                "success": false,
+                "errors": [
+                    "Invalid arguments",
+                    "Missing required property: offset",
+                    "Missing required property: limit"
+                ]
+            }
+        }
+        // return res.send(data2);
     }
 
     @validate
